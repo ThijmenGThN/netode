069026f104bcb19f45ec201dea189432af919453
Thijmen Heuvelink <t.heuvelink@netzozeker.nl>
2021-11-03 15:51:54 +0100

    🐛 Unstable: Settings additions

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,119 +1,107 @@
-import React from "react"
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Link,
-    Switch,
-} from "react-router-dom"
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+  Switch,
+} from "react-router-dom";
 import {
-    FaCouch,
-    FaUserFriends,
-    FaSpa,
-    FaVials,
-    FaStream,
-} from "react-icons/fa"
+  FaCouch,
+  FaUserFriends,
+  FaTicketAlt,
+  FaBox,
+  FaSlidersH,
+} from "react-icons/fa";
 
-import Lounge from "../pages/Lounge"
-import Social from "../pages/Social"
-import Splash from "../pages/Splash"
-import Modules from "../pages/Modules"
-import Settings from "../pages/Settings"
+import Lounge from "../pages/Lounge";
+import Social from "../pages/Social";
+import Splash from "../pages/Splash";
+import Modules from "../pages/Modules";
+import Settings from "../pages/Settings";
 
-import "./styles/NavBar.css"
+import "./styles/NavBar.css";
 
-const $ = (el) => document.querySelector(el)
+const $ = (el) => document.querySelector(el);
 
-let at = 2
+let at = 2;
 const go = (to) => {
-    $("#NavBar-Active").style.left = 19.5 * to + "%"
+  $("#NavBar-Active").style.left = 19.5 * to + "%";
 
-    $("#NavBar-Item" + at).style.backgroundColor = "#1a1f2580"
-    $("#NavBar-Item" + at).style.transform = "scale(1, 1)"
+  $("#NavBar-Item" + at).style.backgroundColor = "#1a1f2580";
+  $("#NavBar-Item" + at).style.transform = "scale(1, 1)";
 
-    $("#NavBar-Item" + to).style.backgroundColor = "#1a1f25bf"
-    $("#NavBar-Item" + to).style.transform = "scale(0.95, 0.9)"
+  $("#NavBar-Item" + to).style.backgroundColor = "#1a1f25bf";
+  $("#NavBar-Item" + to).style.transform = "scale(0.95, 0.9)";
 
-    at = to
-}
+  at = to;
+};
 
 export default () => {
-    return (
-        <Router>
-            <div className="NavBar">
-                <Link to="/lounge">
-                    <button
-                        onClick={() => (at !== 0 ? go(0) : null)}
-                        id="NavBar-Item0"
-                    >
-                        <FaCouch />
-                    </button>
-                </Link>
+  return (
+    <Router>
+      <div className="NavBar">
+        <Link to="/lounge">
+          <button onClick={() => (at !== 0 ? go(0) : null)} id="NavBar-Item0">
+            <FaCouch />
+          </button>
+        </Link>
 
-                <Link to="/social">
-                    <button
-                        onClick={() => (at !== 1 ? go(1) : null)}
-                        id="NavBar-Item1"
-                    >
-                        <FaUserFriends />
-                    </button>
-                </Link>
+        <Link to="/social">
+          <button onClick={() => (at !== 1 ? go(1) : null)} id="NavBar-Item1">
+            <FaUserFriends />
+          </button>
+        </Link>
 
-                <Link to="/splash">
-                    <button
-                        style={{
-                            backgroundColor: "#1a1f25bf",
-                            transform: "scale(0.95, 0.9)",
-                        }}
-                        onClick={() => (at !== 2 ? go(2) : null)}
-                        id="NavBar-Item2"
-                    >
-                        <FaSpa />
-                    </button>
-                </Link>
+        <Link to="/splash">
+          <button
+            style={{
+              backgroundColor: "#1a1f25bf",
+              transform: "scale(0.95, 0.9)",
+            }}
+            onClick={() => (at !== 2 ? go(2) : null)}
+            id="NavBar-Item2"
+          >
+            <FaTicketAlt />
+          </button>
+        </Link>
 
-                <Link to="/modules">
-                    <button
-                        onClick={() => (at !== 3 ? go(3) : null)}
-                        id="NavBar-Item3"
-                    >
-                        <FaVials />
-                    </button>
-                </Link>
+        <Link to="/modules">
+          <button onClick={() => (at !== 3 ? go(3) : null)} id="NavBar-Item3">
+            <FaBox />
+          </button>
+        </Link>
 
-                <Link to="/settings">
-                    <button
-                        onClick={() => (at !== 4 ? go(4) : null)}
-                        id="NavBar-Item4"
-                    >
-                        <FaStream />
-                    </button>
-                </Link>
+        <Link to="/settings">
+          <button onClick={() => (at !== 4 ? go(4) : null)} id="NavBar-Item4">
+            <FaSlidersH />
+          </button>
+        </Link>
 
-                <div id="NavBar-Active"></div>
-            </div>
+        <div id="NavBar-Active"></div>
+      </div>
 
-            <div id="Page">
-                <Switch>
-                    <Route path="/lounge">
-                        <Lounge />
-                    </Route>
-                    <Route path="/social">
-                        <Social />
-                    </Route>
-                    <Route path="/splash">
-                        <Splash />
-                    </Route>
-                    <Route path="/modules">
-                        <Modules />
-                    </Route>
-                    <Route path="/settings">
-                        <Settings />
-                    </Route>
+      <div id="Page">
+        <Switch>
+          <Route path="/lounge">
+            <Lounge />
+          </Route>
+          <Route path="/social">
+            <Social />
+          </Route>
+          <Route path="/splash">
+            <Splash />
+          </Route>
+          <Route path="/modules">
+            <Modules />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
 
-                    <Route render={() => <Redirect to="/splash" />} />
-                </Switch>
-            </div>
-        </Router>
-    )
-}
+          <Route render={() => <Redirect to="/splash" />} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,27 +1,27 @@
-import React from "react"
+import React from "react";
 
-import "./styles/TitleBar.css"
+import "./styles/TitleBar.css";
 
-const { ipcRenderer } = window.require("electron")
+const { ipcRenderer } = window.require("electron");
 
-const exit = () => ipcRenderer.send("exit")
-const minimize = () => ipcRenderer.send("minimize")
+const exit = () => ipcRenderer.send("exit");
+const minimize = () => ipcRenderer.send("minimize");
 
 export default () => {
-    return (
-        <div className="TitleBar">
-            <p className="TitleBar-Title">netode</p>
+  return (
+    <div className="TitleBar">
+      <p className="TitleBar-Title">netode</p>
 
-            <div className="TitleBar-Controls">
-                <button
-                    onClick={minimize}
-                    style={{ backgroundColor: "#c3c3c380" }}
-                ></button>
-                <button
-                    onClick={exit}
-                    style={{ backgroundColor: "#d73250e6", width: "22.5px" }}
-                ></button>
-            </div>
-        </div>
-    )
-}
+      <div className="TitleBar-Controls">
+        <button
+          onClick={minimize}
+          style={{ backgroundColor: "#c3c3c380" }}
+        ></button>
+        <button
+          onClick={exit}
+          style={{ backgroundColor: "#d73250e6", width: "22.5px" }}
+        ></button>
+      </div>
+    </div>
+  );
+};
